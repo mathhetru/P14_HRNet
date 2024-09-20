86c726f7a18eee56fc406c57eaf8e565a5b28183
Mathilde Hetru <76809829+mathhetru@users.noreply.github.com>
2024-09-20 12:34:27 +0200

    feat(front): add lazy loading from react

--- HRNet-project/src/App.tsx
+++ HRNet-project/src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { PrimeReactProvider } from "primereact/api";
 import Header from "./components/Header.tsx";
 import Footer from "./components/Footer.tsx";
-import EmployeeList from "./pages/EmployeeList.tsx";
-import CreateEmployeeView from "./pages/CreateEmployeeView.tsx";
 import "primereact/resources/themes/mira/theme.css";
 import "./styles/style.scss";
+import { lazy } from "react";
+
+const EmployeeList = lazy(() => import("./pages/EmployeeList.tsx"));
+const CreateEmployeeView = lazy(() => import("./pages/CreateEmployeeView.tsx"));
 
 function App() {
   return (
